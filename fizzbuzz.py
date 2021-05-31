5a3115a036e6a4113e9406f654f2876c5bfad9fd
Ryan Zimmerman <zimmerry@oregonstate.edu>
2021-05-31 13:10:59 -0700

    Implemented fizzbuzz method, print 'Buzz' if the number is a multiple of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0):
             print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
         else:
             print(i)
