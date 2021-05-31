0addab66e11fe7f357b39b47f7f67ebfdc86ecdd
Ryan Zimmerman <zimmerry@oregonstate.edu>
2021-05-31 13:29:33 -0700

    Implemented is_leap method such that multiples of 400 return true

--- leap.py
+++ leap.py
@@ -1,7 +1,10 @@
 def is_leap(year):
     if (year % 4 == 0):
         if (year % 100 == 0):
-            return False
+            if (year % 400 == 0):
+                return True
+            else:
+                return False
         else:
             return True
     else:
