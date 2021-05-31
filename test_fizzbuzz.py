44275c2bee78342756c2dd4eb27dd7c1e9b760d3
Ryan Zimmerman <zimmerry@oregonstate.edu>
2021-05-31 13:11:48 -0700

    Wrote fourth test, check that fifteenth line prints 'FizzBuzz'

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -18,3 +18,9 @@ def test_fifth_line(capsys):
 
     output = capsys.readouterr().out.split('\n')[:-1]
     assert output[4] == "Buzz"
+
+def test_fifteeenth_line(capsys):
+    fizzbuzz.fizzbuzz()
+
+    output = capsys.readouterr().out.split('\n')[:-1]
+    assert output[14] == "FizzBuzz"
